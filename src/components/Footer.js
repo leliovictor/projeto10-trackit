c010638cb52ebc83974609029fd4988f5d5c66c1
leliovictor <leliov.vieira@gmail.com>
2022-05-25 20:24:27 -0300

    Added progress circular bar on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,32 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { CircularProgressbar,buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 export default function Footer() {
-  return (
+    
+    const percentage = 50; //Colocar depois valor variavel vindo do useContext;
+
+    return (
     <Div>
       <Link to={"/habitos"}>Hábitos</Link>
+      <Circular>
+      <CircularProgressbar
+        value={percentage}
+        text="Hoje"
+        background
+        backgroundPadding={6}
+        styles={buildStyles({
+          backgroundColor: "#52B6FF",
+          textColor: "#FFFFFF",
+          textSize: "18px",
+          pathColor: "#FFFFFF",
+          trailColor: "transparent",
+          width: "91px",
+          height: "91px"
+        })}
+      />
+      </Circular>
       <Link to={"/historico"}>Histórico</Link>
     </Div>
   );
@@ -32,3 +54,12 @@ const Div = styled.div`
     color: #52b6ff;
   }
 `;
+
+const Circular = styled.div`
+    width: 91px;
+    height: 91px;
+    position: fixed;
+    left: 50%;
+    transform: translate(-50%, 0);
+    bottom: 10px;
+`
